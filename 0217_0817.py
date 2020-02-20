be6a2cee5bc27284a8a662ef1f7b9a5e9e03ba1c
Sumin Kim <supermin@Suminui-MacBookPro.local>
2020-02-20 10:23:33 +0900

    first commit

---
+++ 0217_0817.py
@@ -0,0 +1,705 @@
+#!/usr/bin/env python
+# coding: utf-8
+**Import Packages**
+# In[155]:
+
+
+import re
+import numpy as np
+import pandas as pd
+from pprint import pprint
+
+
+# In[7]:
+
+
+get_ipython().system('sudo pip3 install gensim')
+
+
+# In[156]:
+
+
+# Gensim
+import gensim
+import gensim.corpora as corpora
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel
+
+
+# In[10]:
+
+
+get_ipython().system('sudo pip3 install spacy')
+
+
+# In[157]:
+
+
+# spacy for lemmatization
+import spacy
+
+
+# In[13]:
+
+
+get_ipython().system('sudo pip3 install pyldavis')
+
+
+# In[15]:
+
+
+get_ipython().system('sudo pip3 install matplotlib')
+
+
+# In[158]:
+
+
+# Plotting tools
+import pyLDAvis
+import pyLDAvis.gensim  # don't skip this
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[159]:
+
+
+# Enable logging for gensim - optional
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.ERROR)
+
+import warnings
+warnings.filterwarnings("ignore",category=DeprecationWarning)
+
+
+# In[19]:
+
+
+get_ipython().system('sudo pip3 install nltk')
+
+
+# In[160]:
+
+
+# Download nltk stopwords and spacy model
+import nltk; nltk.download('stopwords')
+
+
+# In[227]:
+
+
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+stop_words.extend(['year', 'com', 'day', 'datum'])
+
+
+# In[228]:
+
+
+print(len(stop_words))
+
+
+# ##**Import Dataset**
+
+# In[229]:
+
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# Import Dataset
+df = pd.read_csv("data_amazon.csv")
+
+
+# In[230]:
+
+
+df.info()
+
+
+# In[209]:
+
+
+pd.set_option('display.max_columns', 100)
+print(df)
+
+
+# In[210]:
+
+
+# Convert email body to list
+data = df.Speech_to_Text.values.tolist()
+
+
+# In[211]:
+
+
+print(data)
+
+
+# In[232]:
+
+
+df = [re.sub('year',"", line) for line in df] #'year', 'com', 'day', 'datum'
+df = [re.sub('com',"", line) for line in df]
+df = [re.sub('day',"", line) for line in df]
+df = [re.sub('datum',"", line) for line in df]
+
+
+# ##**Tokenize words and Clean-up text**
+
+# In[233]:
+
+
+def sent_to_words(sentences):
+    for sentence in sentences:
+        yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
+
+data_words = list(sent_to_words(data))
+
+print(data_words[3])
+
+
+# ##**Creating Bigram and Trigram Models**
+
+# In[234]:
+
+
+from gensim.models.phrases import Phrases, Phraser
+
+# Build the bigram and trigram models
+bigram = Phrases(data_words, min_count=5, threshold=100) # higher threshold fewer phrases.
+trigram = Phrases(bigram[data_words], threshold=100)
+
+# Faster way to get a sentence clubbed as a trigram/bigram
+bigram_mod = Phraser(bigram)
+trigram_mod = Phraser(trigram)
+
+# See trigram example
+print(trigram_mod[bigram_mod[data_words[5]]])
+
+
+# ##**Remove Stopwords, Make Bigrams and Lemmatize**
+
+# In[235]:
+
+
+# Define functions for stopwords, bigrams, trigrams and lemmatization
+def remove_stopwords(texts):
+    return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+
+def make_bigrams(texts):
+    return [bigram_mod[doc] for doc in texts]
+
+def make_trigrams(texts):
+    return [trigram_mod[bigram_mod[doc]] for doc in texts]
+
+def lemmatization(texts, allowed_postags=['NOUN']): # 'ADJ', 'VERB', 'ADV'
+    """https://spacy.io/api/annotation"""
+    texts_out = []
+    for sent in texts:
+        doc = nlp(" ".join(sent)) 
+        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    return texts_out
+
+
+# In[236]:
+
+
+# Remove Stop Words
+data_words_nostops = remove_stopwords(data_words)
+
+
+# In[237]:
+
+
+# Form Bigrams
+data_words_bigrams = make_bigrams(data_words_nostops)
+
+
+# In[193]:
+
+
+get_ipython().system('sudo pip3 install https://github.com/explosion/spacy-models/releases/download/en_core_web_md-2.0.0/en_core_web_md-2.0.0.tar.gz')
+
+
+# In[49]:
+
+
+get_ipython().system('sudo pip3 install -U spacy download en_core_web_sm')
+
+
+# In[247]:
+
+
+import en_core_web_sm
+
+# Initialize spacy 'en' model, keeping only tagger component (for efficiency)
+# python3 -m spacy download en
+nlp = en_core_web_sm.load()
+# nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+
+
+# In[248]:
+
+
+# Do lemmatization keeping only noun, adj, vb, adv
+data_lemmatized = lemmatization(data_words_bigrams, allowed_postags=['NOUN'])
+
+
+# In[249]:
+
+
+print(data_lemmatized[:1])
+
+
+# ##**Create the Dictionary and Corpus needed for Topic Modeling**
+
+# In[250]:
+
+
+# Create Dictionary
+id2word = corpora.Dictionary(data_lemmatized)
+
+# Create Corpus
+texts = data_lemmatized
+
+# Term Document Frequency
+corpus = [id2word.doc2bow(text) for text in texts]
+
+# View
+print(corpus[:1])
+
+
+# In[251]:
+
+
+# Human readable format of corpus (term-frequency)
+[[(id2word[id], freq) for id, freq in cp] for cp in corpus[:1]]
+
+
+# ##**Building the Topic Model**
+
+# In[252]:
+
+
+# Build LDA model
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                           id2word=id2word,
+                                           num_topics=6,
+                                           random_state=100,
+                                           update_every=1,
+                                           chunksize=100,
+                                           passes=10,
+                                           alpha='auto',
+                                           per_word_topics=True)
+
+
+# ##**View the topics in LDA model**
+
+# In[253]:
+
+
+# Print the Keyword in the 10 topics
+pprint(lda_model.print_topics())
+doc_lda = lda_model[corpus]
+
+
+# ##**Compute Model Coherence Score**
+
+# In[254]:
+
+
+# Compute Coherence Score
+coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
+coherence_lda = coherence_model_lda.get_coherence()
+print('\nCoherence Score: ', coherence_lda)
+
+
+# In[255]:
+
+
+# Visualize the topics
+pyLDAvis.enable_notebook(sort=True)
+vis = pyLDAvis.gensim.prepare(lda_model, corpus, id2word)
+pyLDAvis.display(vis)
+
+
+# ##**How to find the optimal number of topics for LDA?**
+
+# In[153]:
+
+
+def compute_coherence_values(dictionary, corpus, texts, limit, start=2, step=3):
+    """
+    Compute c_v coherence for various number of topics
+
+    Parameters:
+    ----------
+    dictionary : Gensim dictionary
+    corpus : Gensim corpus
+    texts : List of input texts
+    limit : Max num of topics
+
+    Returns:
+    -------
+    model_list : List of LDA topic models
+    coherence_values : Coherence values corresponding to the LDA model with respective number of topics
+    """
+    coherence_values = []
+    model_list = []
+    for num_topics in range(start, limit, step):
+        model = gensim.models.ldamodel.LdaModel(corpus=corpus, num_topics=num_topics, id2word=id2word)
+        model_list.append(model)
+        coherencemodel = CoherenceModel(model=model, texts=texts, dictionary=dictionary, coherence='c_v')
+        coherence_values.append(coherencemodel.get_coherence())
+
+    return model_list, coherence_values
+
+
+# In[154]:
+
+
+# Can take a long time to run.
+model_list, coherence_values = compute_coherence_values(dictionary=id2word, corpus=corpus, texts=data_lemmatized, start=2, limit=20, step=2)
+
+
+# In[125]:
+
+
+# Show graph
+limit=20; start=2; step=2;
+x = range(start, limit, step)
+plt.plot(x, coherence_values)
+plt.xlabel("Num Topics")
+plt.ylabel("Coherence score")
+plt.legend(("coherence_values"), loc='best')
+plt.show()
+
+
+# In[127]:
+
+
+# Print the coherence scores
+for m, cv in zip(x, coherence_values):
+    print("Num Topics =", m, " has Coherence Value of", round(cv, 4))
+
+
+# In[78]:
+
+
+print(len(model_list))
+
+
+# In[79]:
+
+
+# Select the model and print the topics
+optimal_model = model_list[2] # Num Topics = 6
+model_topics = optimal_model.show_topics(formatted=False)
+pprint(optimal_model.print_topics(num_words=10))
+
+
+# ##**Finding the dominant topic in each sentence**
+
+# In[256]:
+
+
+def format_topics_sentences(ldamodel=lda_model, corpus=corpus, texts=data):
+    # Init output
+    sent_topics_df = pd.DataFrame()
+
+    # Get main topic in each document
+    for i, row in enumerate(ldamodel[corpus]):
+        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+        # Get the Dominant topic, Perc Contribution and Keywords for each document
+        for j, (topic_num, prop_topic) in enumerate(row):
+            if j == 0:  # => dominant topic
+                wp = ldamodel.show_topic(topic_num)
+                topic_keywords = ", ".join([word for word, prop in wp])
+                sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic,4), topic_keywords]), ignore_index=True)
+            else:
+                break
+    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
+
+    # Add original text to the end of the output
+    contents = pd.Series(texts)
+    sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
+    return(sent_topics_df)
+
+
+df_topic_sents_keywords = format_topics_sentences(ldamodel=optimal_model, corpus=corpus, texts=data)
+
+# Format
+df_dominant_topic = df_topic_sents_keywords.reset_index()
+df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
+
+# Show
+df_dominant_topic.head(10)
+
+
+# ##**Find the most representative document for each topic**
+
+# In[257]:
+
+
+# Group top 5 sentences under each topic
+sent_topics_sorteddf_mallet = pd.DataFrame()
+
+sent_topics_outdf_grpd = df_topic_sents_keywords.groupby('Dominant_Topic')
+
+for i, grp in sent_topics_outdf_grpd:
+    sent_topics_sorteddf_mallet = pd.concat([sent_topics_sorteddf_mallet, 
+                                             grp.sort_values(['Perc_Contribution'], ascending=[0]).head(1)], 
+                                            axis=0)
+
+# Reset Index    
+sent_topics_sorteddf_mallet.reset_index(drop=True, inplace=True)
+
+# Format
+sent_topics_sorteddf_mallet.columns = ['Topic_Num', "Topic_Perc_Contrib", "Keywords", "Text"]
+
+# Show
+sent_topics_sorteddf_mallet.head()
+
+
+# ##**Topic distribution across documents**
+
+# In[258]:
+
+
+# Number of Documents for Each Topic
+topic_counts = df_topic_sents_keywords['Dominant_Topic'].value_counts()
+
+# Percentage of Documents for Each Topic
+topic_contribution = round(topic_counts/topic_counts.sum(), 4)
+
+# Topic Number and Keywords
+topic_num_keywords = df_topic_sents_keywords[['Dominant_Topic', 'Topic_Keywords']]
+
+# Concatenate Column wise
+df_dominant_topics = pd.concat([topic_num_keywords, topic_counts, topic_contribution], axis=1)
+
+# Change Column names
+df_dominant_topics.columns = ['Dominant_Topic', 'Topic_Keywords', 'Num_Documents', 'Perc_Documents']
+
+# Show
+df_dominant_topics
+
+
+# #**Visualization**
+
+# ###**Word CLouds of Top N Keywords in each topic**
+
+# In[90]:
+
+
+get_ipython().system('sudo pip3 install wordcloud')
+
+
+# In[272]:
+
+
+# 1. Wordcloud of Top N words in each topic
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.colors as mcolors
+
+cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
+
+cloud = WordCloud(stopwords=stop_words,
+                  background_color='white',
+                  width=2500,
+                  height=1800,
+                  max_words=10,
+                  colormap='tab10',
+                  color_func=lambda *args, **kwargs: cols[i],
+                  prefer_horizontal=1.0)
+
+topics = lda_model.show_topics(formatted=False)
+
+fig, axes = plt.subplots(2, 3, figsize=(10,10), sharex=True, sharey=True)
+
+for i, ax in enumerate(axes.flatten()):
+    fig.add_subplot(ax)
+    topic_words = dict(topics[i][1])
+    cloud.generate_from_frequencies(topic_words, max_font_size=300)
+    plt.gca().imshow(cloud)
+    plt.gca().set_title('Topic ' + str(i), fontdict=dict(size=16))
+    plt.gca().axis('off')
+
+
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.axis('off')
+plt.margins(x=0, y=0)
+plt.tight_layout()
+plt.show()
+
+
+# ###**Word Counts of Topic Keywords**
+
+# In[260]:
+
+
+from collections import Counter
+topics = lda_model.show_topics(formatted=False)
+data_flat = [w for w_list in  data_lemmatized for w in w_list]
+counter = Counter(data_flat)
+
+out = []
+for i, topic in topics:
+    for word, weight in topic:
+        out.append([word, i , weight, counter[word]])
+
+df = pd.DataFrame(out, columns=['word', 'topic_id', 'importance', 'word_count'])        
+
+# Plot Word Count and Weights of Topic Keywords
+fig, axes = plt.subplots(2, 3, figsize=(15,10), sharey=True, dpi=80)
+cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]
+for i, ax in enumerate(axes.flatten()):
+    ax.bar(x='word', height="word_count", data=df.loc[df.topic_id==i, :], color=cols[i], width=0.5, alpha=0.3, label='Word Count')
+    ax_twin = ax.twinx()
+    ax_twin.bar(x='word', height="importance", data=df.loc[df.topic_id==i, :], color=cols[i], width=0.2, label='Weights')
+    ax.set_ylabel('Word Count', color=cols[i])
+    ax_twin.set_ylim(0, 0.030); ax.set_ylim(0, 3500)
+    ax.set_title('Topic: ' + str(i), color=cols[i], fontsize=16)
+    ax.tick_params(axis='y', left=False)
+    ax.set_xticklabels(df.loc[df.topic_id==i, 'word'], rotation=30, horizontalalignment= 'right')
+    ax.legend(loc='upper left'); ax_twin.legend(loc='upper right')
+
+fig.tight_layout(w_pad=2)    
+fig.suptitle('Word Count and Importance of Topic Keywords', fontsize=22, y=1.05)    
+plt.show()
+
+
+# ###**Sentence Chart Colored by Topic**
+
+# In[269]:
+
+
+# Sentence Coloring of N Sentences
+from matplotlib.patches import Rectangle
+
+def sentences_chart(lda_model=lda_model, corpus=corpus, start = 0, end = 21):
+    corp = corpus[start:end]
+    mycolors = [color for name, color in mcolors.TABLEAU_COLORS.items()]
+
+    fig, axes = plt.subplots(end-start, 1, figsize=(20, (end-start)*0.95), dpi=40)       
+    axes[0].axis('off')
+    for i, ax in enumerate(axes):
+        if i > 0:
+            corp_cur = corp[i-1] 
+            topic_percs, wordid_topics, wordid_phivalues = lda_model[corp_cur]
+            word_dominanttopic = [(lda_model.id2word[wd], topic[0]) for wd, topic in wordid_topics]    
+            ax.text(0.01, 0.5, "Doc " + str(i-1) + ": ", verticalalignment='center',
+                    fontsize=16, color='black', transform=ax.transAxes, fontweight=40)
+
+            # Draw Rectange
+            topic_percs_sorted = sorted(topic_percs, key=lambda x: (x[1]), reverse=True)
+            ax.add_patch(Rectangle((0.0, 0.05), 0.99, 0.90, fill=None, alpha=1, 
+                                   color=mycolors[topic_percs_sorted[0][0]], linewidth=2))
+
+            word_pos = 0.06
+            for j, (word, topics) in enumerate(word_dominanttopic):
+                if j < 14:
+                    ax.text(word_pos, 0.5, word,
+                            horizontalalignment='left',
+                            verticalalignment='center',
+                            fontsize=16, color=mycolors[topics],
+                            transform=ax.transAxes, fontweight=700)
+                    word_pos += .009 * len(word)  # to move the word for the next iter
+                    ax.axis('off')
+            ax.text(word_pos, 0.5, '. . .',
+                    horizontalalignment='left',
+                    verticalalignment='center',
+                    fontsize=16, color='black',
+                    transform=ax.transAxes)       
+
+    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.suptitle('Sentence Topic Coloring for Documents: ' + str(start) + ' to ' + str(end-2), fontsize=22, y=0.95, fontweight=700)
+    plt.tight_layout()
+    plt.show()
+
+sentences_chart()
+
+
+# ###**Number of Documents by Dominant Topic, Weightage**
+
+# In[262]:
+
+
+# Sentence Coloring of N Sentences
+def topics_per_document(model, corpus, start=0, end=1):
+    corpus_sel = corpus[start:end]
+    dominant_topics = []
+    topic_percentages = []
+    for i, corp in enumerate(corpus_sel):
+        topic_percs, wordid_topics, wordid_phivalues = model[corp]
+        dominant_topic = sorted(topic_percs, key = lambda x: x[1], reverse=True)[0][0]
+        dominant_topics.append((i, dominant_topic))
+        topic_percentages.append(topic_percs)
+    return(dominant_topics, topic_percentages)
+
+dominant_topics, topic_percentages = topics_per_document(model=lda_model, corpus=corpus, end=-1)            
+
+# Distribution of Dominant Topics in Each Document
+df = pd.DataFrame(dominant_topics, columns=['Document_Id', 'Dominant_Topic'])
+dominant_topic_in_each_doc = df.groupby('Dominant_Topic').size()
+df_dominant_topic_in_each_doc = dominant_topic_in_each_doc.to_frame(name='count').reset_index()
+
+# Total Topic Distribution by actual weight
+topic_weightage_by_doc = pd.DataFrame([dict(t) for t in topic_percentages])
+df_topic_weightage_by_doc = topic_weightage_by_doc.sum().to_frame(name='count').reset_index()
+
+# Top 3 Keywords for each Topic
+topic_top3words = [(i, topic) for i, topics in lda_model.show_topics(formatted=False) 
+                                 for j, (topic, wt) in enumerate(topics) if j < 7]
+
+df_top3words_stacked = pd.DataFrame(topic_top3words, columns=['topic_id', 'words'])
+df_top3words = df_top3words_stacked.groupby('topic_id').agg(', \n'.join)
+df_top3words.reset_index(level=0,inplace=True)
+
+
+# In[263]:
+
+
+from matplotlib.ticker import FuncFormatter
+
+# Plot
+fig, (ax1, ax2) = plt.subplots(1,2,figsize=(10, 4), dpi=120, sharey=True)
+
+# Topic Distribution by Dominant Topics
+ax1.bar(x='Dominant_Topic', height='count', data=df_dominant_topic_in_each_doc, width=.5, color='firebrick')
+ax1.set_xticks(range(df_dominant_topic_in_each_doc.Dominant_Topic.unique().__len__()))
+tick_formatter = FuncFormatter(lambda x, pos: 'Topic ' + str(x)+ '\n' + df_top3words.loc[df_top3words.topic_id==x, 'words'].values[0])
+ax1.xaxis.set_major_formatter(tick_formatter)
+ax1.set_title('Number of Documents by Dominant Topic', fontdict=dict(size=10))
+ax1.set_ylabel('Number of Documents')
+ax1.set_ylim(0, 15)
+
+
+# Topic Distribution by Topic Weights
+ax2.bar(x='index', height='count', data=df_topic_weightage_by_doc, width=.5, color='steelblue')
+ax2.set_xticks(range(df_topic_weightage_by_doc.index.unique().__len__()))
+ax2.xaxis.set_major_formatter(tick_formatter)
+ax2.set_title('Number of Documents by Topic Weightage', fontdict=dict(size=10))
+
+plt.show()
+
+
+# ###**pyLDAVis**
+
+# In[112]:
+
+
+import pyLDAvis.gensim
+pyLDAvis.enable_notebook()
+vis = pyLDAvis.gensim.prepare(lda_model, corpus, dictionary=lda_model.id2word)
+vis
+
+
+# In[ ]:
+
+
+
+
